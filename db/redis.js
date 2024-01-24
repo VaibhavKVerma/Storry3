467881cc402838a292fffdcf86fee2389fab0360
Vaibhav Kumar Verma <76198333+VaibhavKVerma@users.noreply.github.com>
2024-01-24 12:26:29 +0530

    Redis integration done (#3)
    Co-authored-by: Vaibhav <vaibhav.verma1@whitehatjr.com>

--- db/redis.js
+++ db/redis.js
@@ -3,6 +3,8 @@ const resolve = require("path").resolve;
 const dotEnvPath = resolve(".env");
 require("dotenv").config({ path: dotEnvPath });
 
+const defaultExpiryInSeconds = 600;
+
 module.exports = () => {
   const client = redis.createClient({
     password: process.env.REDIS_PASSWORD,
@@ -15,6 +17,21 @@ module.exports = () => {
   client.on("error", (err) => {
     console.error(`Error connecting to Redis: ${err}`);
   });
+
+  const checkConnection = async () => {
+    try {
+      await client.connect();
+      const reply = await client.ping();
+      console.log(`Redis is connected: ${reply === "PONG"}`);
+    } catch (err) {
+      console.error(`Error with ping command: ${err}`);
+    }
+  }
+
+  const closeConnection = () => {
+    client.quit();
+    console.log("Closed Redis connection");
+  };
 
   const _constructKey = (cacheName, key) => cacheName + "_" + key;
 
@@ -46,6 +63,8 @@ module.exports = () => {
 
   return {
     setKey,
-    getKey
-  }
+    getKey,
+    checkConnection,
+    closeConnection
+  };
 };
--- index.js
+++ index.js
@@ -9,12 +9,35 @@ const redis = require("./db/redis");
 
 const init = async () => {
   try {
-    await mongoDb();
-    redis();
+    // Initialize MongoDB
+    const mongoDbClient = mongoDb();
+    await mongoDbClient.connectToMongoDb();
+
+    // Initialize Redis
+    const redisClient = redis();
+    await redisClient.checkConnection();
+
+    process.on('SIGINT', async () => {
+      await mongoDbClient.closeMongoDbConnection();
+      redisClient.closeConnection();
+      process.exit(0);
+    });
+
+    process.on('SIGTERM', async () => {
+      await mongoDbClient.closeMongoDbConnection();
+      redisClient.closeConnection();
+      process.exit(0);
+    });
+
+    
+
     app.listen(process.env.PORT_NUMBER, () => {
       console.log("Connection Started on PORT", process.env.PORT_NUMBER);
     });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Initialization error:", error);
+    process.exit(1);
+  }
 };
 
 init();

b6567d7db9ebf58f1ddb494c90be2f27562e004f
Vaibhav <vaibhav.verma1@whitehatjr.com>
2024-01-24 12:25:19 +0530

    Redis integration done

--- db/mongodb.js
+++ db/mongodb.js
@@ -5,12 +5,28 @@ require("dotenv").config({ path: dotEnvPath });
 
 const mongoDbUri = process.env.MONGO_DB_URI;
 
-module.exports = async () => {
-  try {
-    console.log("Connecting to MongoDB");
-    await mongoose.connect(mongoDbUri);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log("Error connecting to MongoDB: ", error.message);
-  }
+module.exports = () => {
+  const connectToMongoDb = async () => {
+    try {
+      await mongoose.connect(mongoDbUri);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    }
+  };
+
+  const closeMongoDbConnection = async () => {
+    try {
+      await mongoose.connection.close();
+      console.log("Closed MongoDB connection");
+    } catch (error) {
+      console.error("Error closing MongoDB connection:", error);
+    }
+  };
+
+  return {
+    connectToMongoDb,
+    closeMongoDbConnection,
+  };
 };
